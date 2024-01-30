a16e51c0bfdea6f6ea54e6f4b60c25f306aa4248
Alex Mickelson <alexmickelson96@gmail.com>
2024-01-30 10:40:00 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,16 @@
 
 cd /home/alex/deployment/file-upload-blazor
 
+echo "cleaning up old projects"
+
 rm -rf bin
 rm -rf obj
 git pull 
+
+echo "building and publishing dll"
 dotnet publish
+
+echo "doing systemd stuff"
 
 sudo cp alex-upload.service /etc/systemd/system/alex-upload.service
 
@@ -14,5 +20,9 @@ sudo chmod 700 /etc/systemd/system/alex-upload.service
 sudo systemctl daemon-reload
 
 sudo systemctl restart alex-upload
+
+echo "done doing systemd stuff"
+
+systemctl status alex-upload
 
 
